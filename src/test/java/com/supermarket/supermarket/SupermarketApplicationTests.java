3bdf635915b3f5a6553b9f3916a5a482914a36ff
AhmedAtef <ahmedatef00>
2021-07-12 15:29:42 +0200

    add unit testing for create and findall supermarkets

--- src/test/java/com/supermarket/supermarket/SupermarketApplicationTests.java
+++ src/test/java/com/supermarket/supermarket/SupermarketApplicationTests.java
@@ -1,13 +1,49 @@
 package com.supermarket.supermarket;
 
+import com.supermarket.supermarket.entity.Supermarket;
+import com.supermarket.supermarket.repository.SupermarketRepository;
+import com.supermarket.supermarket.service.impl.SupermarketService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
 
 @SpringBootTest
 class SupermarketApplicationTests {
+    @Autowired
+    private SupermarketService supermarketService;
+    @MockBean
+    private SupermarketRepository supermarketRepository;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    @DisplayName("Test FindAll")
+    void testFindAll() {
+        Supermarket supermarket1 = new Supermarket(1L, "احمد", "atef", "address", "img.png", false);
+        Supermarket supermarket2 = new Supermarket(2L, "احمد2", "atef2", "2address", "2img.png", false);
+        doReturn(Arrays
+                .asList(supermarket1, supermarket2)).when(supermarketRepository).findAll();
+        List<Supermarket> supermarkets = supermarketService.getAllSuperMarkets();
+        Assertions.assertEquals(2, supermarkets.size());
 
+    }
+
+    @Test
+    @DisplayName("Test Save Supermarket")
+    void testSave() {
+        Supermarket supermarket1 = new Supermarket(1L, "احمد", "atef", "address", "img.png", false);
+        doReturn(supermarket1)
+                .when(supermarketRepository).save(any());
+        Supermarket returnedSupermarket = supermarketService.createSuperMarket(supermarket1);
+        Assertions.assertNotNull(supermarket1,"notnull");
+        Assertions.assertEquals(1, returnedSupermarket.getId(),"id");
+
+    }
 }
